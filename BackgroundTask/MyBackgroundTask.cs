f91a2d3667401067caa260dc054bab9aa8d6f781
v-liuson <v-liuson@MSDNUWPSERVER1>
2017-01-05 13:46:08 +0800

    Add project files.

---
+++ BackgroundTask/MyBackgroundTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace BackgroundTask
+{
+    public sealed class MyBackgroundTask: XamlRenderingBackgroundTask
+    {
+        protected override void OnRun(IBackgroundTaskInstance taskInstance)
+        {
+            base.OnRun(taskInstance);
+        }
+    }
+}
---
+++ BackgroundTaskSupport/MainPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BackgroundTaskSupport
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RegisterBackgroundTask(object sender, RoutedEventArgs e)
+        {
+            var task = RegisterBackgroundTask("EntryPoint",
+                                              "name",
+                                              new SystemTrigger(SystemTriggerType.TimeZoneChange, false),
+                                              null);
+            AttachProgressAndCompletedHandlers(task);
+            UpdateUI();
+        }
+        public static BackgroundTaskRegistration RegisterBackgroundTask(String taskEntryPoint, String name, IBackgroundTrigger trigger, IBackgroundCondition condition)
+        {
+
+            // If the user denies access, the task will not run.
+            var requestTask = BackgroundExecutionManager.RequestAccessAsync();
+            var builder = new BackgroundTaskBuilder();
+
+            builder.Name = name;
+            builder.TaskEntryPoint = taskEntryPoint;
+            builder.SetTrigger(trigger);
+
+            if (condition != null)
+            {
+                builder.AddCondition(condition);
+                // If the condition changes while the background task is executing then it will
+                // be canceled.
+                builder.CancelOnConditionLoss = true;
+            }
+            BackgroundTaskRegistration task = builder.Register();
+            return task;
+        }
+
+        private void AttachProgressAndCompletedHandlers(IBackgroundTaskRegistration task)
+        {
+            task.Progress += new BackgroundTaskProgressEventHandler(OnProgress);
+            task.Completed += new BackgroundTaskCompletedEventHandler(OnCompleted);
+        }
+
+        private void OnProgress(IBackgroundTaskRegistration task, BackgroundTaskProgressEventArgs args)
+        {
+            var ignored = Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                UpdateUI();
+            });
+        }
+        private void OnCompleted(IBackgroundTaskRegistration task, BackgroundTaskCompletedEventArgs args)
+        {
+            UpdateUI();
+        }
+
+        private async void UpdateUI()
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+            () =>
+            {
+                //code to change UI
+            });
+        }
+    }
+}
